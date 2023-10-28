215b8fbabdf43c5c4080d22f3870b4d6ce691573
Houda Slassi <houdaslassi.18@gmail.com>
2023-10-28 21:21:14 +0100

    chore: index.php

--- src/index.php
+++ src/index.php
@@ -5,17 +5,21 @@ require_once __DIR__ .'/../vendor/autoload.php';
 use App\OOP\Car;
 use App\OOP\Order;
 use App\OOP\CarDashboard;
+use App\OOP\Employee;
+use App\OOP\Salary;
 
-$hyundai = new \App\OOP\Hyundai(100, 'red', 'manual', 'hyundai', '4');
+/*$hyundai = new \App\OOP\Hyundai(100, 'red', 'manual', 'hyundai', '4');
 $dashboard = new CarDashboard();
 $dashboard->setOilLevel(100);
 $dashboard->setFuelLevel(100);
 $dashboard->setAirPressure(100);
 $hyundai->installDashboard($dashboard);
-var_dump($hyundai->carInfo());
+var_dump($hyundai->carInfo());*/
 //var_dump($hyundai->accelerate(100));
 
-$order = new Order('casa', 'meknes', 10);
-$deliveredOrder = $order->deliverOrder($hyundai);
+$houda = new Employee('houda',30,new Salary(1000,0.2,0,0,0,0));
+var_dump($houda->getSalary()->calculateSalary());
+//$order = new Order('casa', 'meknes', 10);
+//$deliveredOrder = $order->deliverOrder($hyundai);
 
-var_dump($deliveredOrder);
+//var_dump($deliveredOrder);
